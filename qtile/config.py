27396591a017519b1a7db6192b07376394984f69
PracticeZone <134071717+PracticeZone@users.noreply.github.com>
2023-07-29 17:29:17 +0200

    Create config.py

---
+++ qtile/config.py
@@ -0,0 +1,311 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import subprocess
+from libqtile import bar, layout, widget,hook,extension
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+import os
+import psutil
+
+
+
+
+
+mod = "mod4"
+terminal = guess_terminal()
+
+keys = [
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    # Switch between windows
+    Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "shift"], "e", lazy.shutdown(), desc="Shutdown Qtile"),
+    # Key([mod], "d", lazy.run_extension(extension.DmenuRun(
+    #     dmenu_prompt=">",
+    #     dmenu_font="Andika-48",
+    #     background="#15181a",
+    #     foreground="#00ff00",
+    #     selected_background="#079822",
+    #     selected_foreground="#fff",
+    # )), desc="Spawn a command using a prompt widget"),
+    Key([mod], "d", lazy.spawn("rofi -show drun"), desc=""),
+    Key([mod,"shift"], "d", lazy.spawn(f"sh {os.path.expanduser('~/.local/bin/custom_commands.sh')}"), desc=""),
+
+    Key(["mod1"], "Tab", lazy.spawn("rofi -show windowcd"), desc=""),
+
+]
+
+def window_to_previous_screen(qtile, switch_group=False, switch_screen=False):
+    i = qtile.screens.index(qtile.current_screen)
+    if i != 0:
+        previous = i - 1
+    else:
+        previous =  len(qtile.screens)-1
+
+    group = qtile.screens[previous].group.name
+    qtile.current_window.togroup(group, switch_group=switch_group)
+    if switch_screen == True:
+        qtile.cmd_to_screen(previous)
+
+def window_to_next_screen(qtile, switch_group=False, switch_screen=False):
+    i = qtile.screens.index(qtile.current_screen)
+    if i + 1 != len(qtile.screens):
+        next = i + 1
+    else:
+        next = 0
+    
+    group = qtile.screens[next].group.name
+    qtile.current_window.togroup(group, switch_group=switch_group)
+    if switch_screen == True:
+        qtile.cmd_to_screen(next)
+
+
+keys.extend([
+    
+    Key([mod,"mod1"],"Right",  lazy.function(window_to_next_screen, switch_screen=True)),
+    Key([mod,"mod1"],"Left", lazy.function(window_to_previous_screen, switch_screen=True)),
+    Key([mod, "mod1"], 'Up', lazy.next_screen(), desc='Next monitor'),
+])
+
+
+groups = [Group(i) for i in "123456789"]
+
+for i in groups:
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod1 + shift + letter of group = move focused window to group
+            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            #     desc="move focused window to group {}".format(i.name)),
+        ]
+    )
+
+layouts = [
+    # layout.Tile(),
+    layout.Max(),
+    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2),
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadTall(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font="JetBrainsMono Nerd Font Bold",
+    fontsize=26,
+    padding=16,
+)
+extension_defaults = widget_defaults.copy()
+BATTERYLEVELS = ["󰂃","󰁺","󰁻","󰁼","󰁽","󰁾","󰁿","󰂀","󰂁","󰂂","󰁹"]
+BATTERYLEVELSCHARGING = ["󰢟","󰢜","󰂆","󰂇","󰂈","󰢝","󰂉","󰢞","󰂊","󰂋","󰂅"]
+def getBatteryLevelIcon(charging=False):
+    battery = psutil.sensors_battery()
+    if charging:
+        return BATTERYLEVELSCHARGING[round(battery.percent/10)]
+    else:
+        return BATTERYLEVELS[round(battery.percent/10)]
+
+
+screens = [
+    Screen(
+        wallpaper='~/wallpapers/wallpaper1.jpg',
+        wallpaper_mode='fill',
+        top=bar.Bar(
+            [
+                
+                widget.CurrentLayout(background="9DBCFF", foreground="00287F"),
+                widget.GroupBox(highlight_method='line', background="98A4FF",active="00129D",highlight_color=['98A4FF', '7989FF'],inactive="5A6DFF",block_highlight_text_color="00129D",this_screen_border="5A6DFF", this_current_screen_border="1B36FF",other_screen_border="5A6DFF", other_current_screen_border="1B36FF"),
+                widget.Prompt(background="B298FF",foreground="1F007D"),
+                widget.WindowName(background="B298FF",foreground="1F007D"),
+                widget.Chord(
+                    chords_colors={
+                        "launch": ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+                # widget.StatusNotifier(),
+                widget.Battery(format="{char} {percent:2.0%} {hour:d}h{min:02d}m",discharge_char=getBatteryLevelIcon(),charge_char=getBatteryLevelIcon(True),empty_char="󰂃",update_interval=3,background="DAA6FF",foreground="4C0082"),
+                widget.CPU(format= "\uf4bc {load_percent}%", background="FFB5FF",foreground="860086"),#{freq_current}GHz
+                widget.Memory(format = "\ue266 {MemUsed: .2f}{mm}",measure_mem='G', background="DAA6FF",foreground="4C0082"),#/{MemTotal: .0f}{mm}
+                widget.Net(format = "{interface} 󰈀 {down: .1f}MB ↓↑{up: .1f}MB",prefix='M', background="B298FF",foreground="1F007D"),
+                widget.Wlan(format=" {essid}",disconnected_message="󰖪",background="B298FF",foreground="1F007D"),
+                widget.ThermalSensor(format = "\uf2c7 {temp:.1f}{unit}", background="98A4FF",foreground="00129D"),
+                #widget.NvidiaSensors(background="98A4FF",foreground="00129D"),
+                
+                widget.Clock(format="󱛡 %a %d.%b.%y  %H:%M", background="9DBCFF", foreground="00287F"),
+                #widget.Systray(),
+                # widget.QuickExit(),
+            ],
+            48,
+            #border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            #border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+    ),    
+    Screen(
+        wallpaper='~/wallpapers/wallpaper2.png',
+        wallpaper_mode='fill',
+        top=bar.Bar(
+            [
+                
+                widget.CurrentLayout(background="9DBCFF", foreground="00287F"),
+                widget.GroupBox(highlight_method='line', background="98A4FF",active="00129D",highlight_color=['98A4FF', '7989FF'],inactive="5A6DFF",block_highlight_text_color="00129D",this_screen_border="5A6DFF", this_current_screen_border="1B36FF",other_screen_border="5A6DFF", other_current_screen_border="1B36FF"),
+                widget.Prompt(background="B298FF",foreground="1F007D"),
+                widget.WindowName(background="B298FF",foreground="1F007D"),
+                widget.Chord(
+                    chords_colors={
+                        "launch": ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+                # widget.StatusNotifier(),
+                widget.Battery(format="{char} {percent:2.0%} {hour:d}h{min:02d}m",discharge_char=getBatteryLevelIcon(),charge_char=getBatteryLevelIcon(True),empty_char="󰂃",update_interval=3,background="DAA6FF",foreground="4C0082"),
+                widget.CPU(format= "\uf4bc {load_percent}%", background="FFB5FF",foreground="860086"),#{freq_current}GHz
+                widget.Memory(format = "\ue266 {MemUsed: .2f}{mm}",measure_mem='G', background="DAA6FF",foreground="4C0082"),#/{MemTotal: .0f}{mm}
+                widget.Net(format = "{interface} 󰈀 {down: .1f}MB ↓↑{up: .1f}MB",prefix='M', background="B298FF",foreground="1F007D"),
+                widget.Wlan(format=" {essid}",disconnected_message="󰖪",background="B298FF",foreground="1F007D"),
+                widget.ThermalSensor(format = "\uf2c7 {temp:.1f}{unit}", background="98A4FF",foreground="00129D"),
+                #widget.NvidiaSensors(background="98A4FF",foreground="00129D"),
+                
+                widget.Clock(format="󱛡 %a %d.%b.%y  %H:%M", background="9DBCFF", foreground="00287F"),
+                #widget.Systray(),
+                # widget.QuickExit(),
+            ],
+            48,
+            #border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            #border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+        )
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
+
+
+
+
+@hook.subscribe.startup_once
+def autostart():
+    subprocess.Popen("xrandr --output eDP-1 --primary --mode 3000x2000 --pos 0x1688 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --scale 1.5625x1.5625 --pos 0x0 --rotate normal --output DP-4 --off", shell=True)
+    subprocess.Popen("xrandr --dpi 170", shell=True)
+
+
